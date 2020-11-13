b50567bc0e3417b4ca0e5f4e50787231c9677278
anhthuvu <18anhv0512@alfholsskoli.is>
2020-11-13 13:02:47 +0000

    fetch data

--- src/pages/List.svelte
+++ src/pages/List.svelte
@@ -1,6 +1,13 @@
 <script>
     let pageName="List";
     import {Link} from "svelte-routing";
+    import { onMount } from "svelte";
+
+    let events = [];
+    onMount(async () => {
+        const res = await fetch(`https://gist.githubusercontent.com/anhthuvu/02012661b6fee976842f035be56a3ded/raw/5da4fb30af059d268715fbf9681901f131420328/data.json`);
+        events = await res.json();
+    });
 </script>
 
 <main>  
@@ -8,4 +15,16 @@
         <Link back>Back</Link>
         <h1 class="font-bold text-4xl">{pageName}</h1>
     </header> 
+    <div>
+        {#each events as event}
+        <ul>
+            <li><b>{event.name}</b></li>
+            <li>{event.date}</li>
+            <li>{event.location}</li>
+            <li><img src={event.imageSource} alt="imaage" width="500"/></li>
+        </ul>
+        {:else}
+        <p>loading...</p>
+        {/each}
+    </div>
 </main>
